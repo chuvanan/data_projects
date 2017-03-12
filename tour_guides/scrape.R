aa8c3887dc693bd073d9baaea9e39ffd9bd35976
An Chu <chuvanan.cva@gmail.com>
2017-03-12 07:23:05 +0700

    add script and database of all registered tour guides in Vietnam

---
+++ tour_guides/scrape.R
@@ -0,0 +1,28 @@
+
+
+library(rvest)
+library(stringr)
+
+
+baseurl <- "http://2016.huongdanvien.vn/index.php/guide/cat/05/"
+n_page <- 370
+pages <- paste0(baseurl, 1:370)
+
+download_hdvdl_profile <- function(urls) {
+  require(rvest)
+  require(stringr)
+  out <- character(0)
+  for (i in urls) {
+    site <- read_html(i)
+    site <- html_nodes(site, xpath = "//div[@class='guide-info']")
+    site <- html_text(site)
+    site <- str_replace_all(site, "[\r\n\t]", "")
+    site <- trimws(site, which = "both")
+    out <- c(out, site)
+  }
+  out
+}
+
+ds_hdvl <- download_hdvdl_profile(pages)
+
+## write.csv(ds_hdvl, "~/Documents/data_projects/tour_guides/tour_guides_rawdb.csv", row.names = F)
