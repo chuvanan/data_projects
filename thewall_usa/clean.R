f0baee23bd6496e7ff91a620a7eb396691ca6f70
AnChu <chuvanan.cva@gmail.com>
2018-06-09 23:16:04 +0700

    User closure to make functions for extracting data fields

--- thewall_usa/clean.R
+++ thewall_usa/clean.R
@@ -4,142 +4,39 @@ library(purrr)
 
 thewall <- readRDS("thewall_usa.rds")
 
-## full name
-## army unit
-## background
-## origin
-## length of service
-## service start date
-## casualty date
-## casualty place
-## casualty type
-## casualty detail
-## body recovered
-## panel line
-
-get_name <- function(x) {
-    purrr::map_chr(x, 1L)
-}
-
-get_army_unit <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("(Regular)$|(Service)$|(Reserve)$", s)]
-        s <- if (length(s) == 0L) NA else s
-        s
+get_field <- function(regx = NULL, pos = NULL, ...) {
+    function(x) {
+        fun <- function(s) {
+            s <- s[grepl(regx, s, ...)]
+            s <- if (length(s) == 0L) NA else s
+            s
+        }
+        fun <- if (is.null(pos)) fun else function(v) v[[pos]]
+        res <- lapply(x, fun)
+        unlist(res)
     }
-    purrr::map_chr(x, fun)
 }
 
-get_background <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("year old", s)]
-        s <- if (length(s) == 0L) NA else s
-        s
-    }
-    purrr::map_chr(x, fun)
-}
-
-get_dob <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("^Born on", s)]
-        s <- if (length(s) == 0L) NA else s
-        s
-    }
-    purrr::map_chr(x, fun)
-}
-
-get_length_of_service <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("Length of service", s)]
-        s <- if (length(s) == 0L) NA else s
-        s
-    }
-    purrr::map_chr(x, fun)
-}
-
-get_service_start_date <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("tour began", s)]
-        s <- if (length(s) == 0L) NA else s
-        s
-    }
-    purrr::map_chr(x, fun)
-}
-
-get_casualty_date <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("Casualty was on", s)]
-        s[grepl("Length of service", s)]
-        s
-    }
-    purrr::map_chr(x, fun)
-}
-
-get_body_recovered <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("(Body).*(recovered)", s)]
-        s <- if (length(s) == 0L) NA else s
-        s
-    }
-    purrr::map_chr(x, fun)
-}
-
-get_casualty_location <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("^In.*(VIETNAM|LAOS|CAMBODIA)", s)]
-        s <- if (length(s) == 0L) NA else s
-        s
-    }
-    purrr::map_chr(x, fun)
-}
-
-get_casualty_type <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("^(non-hostile)|(hostile)", s, ignore.case = TRUE)]
-        s <- if (length(s) == 0L) NA else s
-        s
-    }
-    purrr::map_chr(x, fun)
-}
-
-get_casualty_detail <- function(x) {
-    fun <- function() NULL
-}
-
-get_panel_line <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("^Panel", s)]
-        s <- if (length(s) == 0L) NA else s
-        s
-    }
-    purrr::map_chr(x, fun)
-}
-
-get_birth_place <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("^From", s, ignore.case = TRUE)]
-        s <- if (length(s) == 0L) NA else s
-        s
-    }
-    purrr::map_chr(x, fun)
-}
-
-get_division <- function(x) {
-    fun <- function(s) {
-        s <- s[grepl("^([1-9]+(st|nd|rd|th))", s)]
-        s <- if (length(s) == 0L) NA else s
-        s
-    }
-    purrr::map_chr(x, fun)
-}
-
+get_name <- get_field(pos = 1L)
+get_service_unit <- get_field(regx = "(Regular|Reserve|Service|Temporary|National Guard)$")
+get_birth_date <- get_field(regx = "^Born on")
+get_length_of_service <- get_field(regx = "Length of service")
+get_service_start_date <- get_field(regx = "tour began")
+get_casualty_date <- get_field(regx = "Casualty was on")
+get_body_recovered <- get_field(regx = "(Body).*(recovered)")
+get_casualty_location <- get_field(regx = "^In.*(VIETNAM|LAOS|CAMBODIA)")
+get_casualty_type <- get_field(regx = "^(non-hostile)|(hostile)", ignore.case = TRUE)
+get_panel_line <- get_field(regx = "^Panel")
+get_birth_place <- get_field(regx = "^From")
+get_rank_rate <- get_field(regx = "^([1-9]+(st|nd|rd|th))")
+get_background <- get_field(regx = "year old")
 
 sample(thewall, 10)
 
 get_name(thewall)
-get_army_unit(thewall)
+get_service_unit(thewall)
 get_background(thewall)
-get_dob(thewall)
+get_birth_date(thewall)
 get_length_of_service(thewall)
 get_service_start_date(thewall)
 get_casualty_date(thewall)
@@ -149,4 +46,4 @@ get_casualty_type(thewall)
 ## get_casualty_detail(thewall)
 get_panel_line(thewall)
 get_birth_place(thewall)
-get_division(thewall)
+get_rank_rate(thewall)
