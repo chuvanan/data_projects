3d206c56b41cb0292f36af4f7954574ef9af13fb
AnChu <chuvanan.cva@gmail.com>
2018-06-09 13:11:21 +0700

    Export a small sample of 100 pages

--- thewall_usa/crawler.R
+++ thewall_usa/crawler.R
@@ -5,33 +5,33 @@ library(purrr)
 library(stringr)
 
 ## n_pages <- 58223
-n_pages <- 10000
+n_pages <- 100
 base_url <- "http://thewall-usa.com/info.asp?recid="
 urls <- paste0(base_url, 1:n_pages)
 
 crawler <- function(.url) {
 
-  # read html
-  page <- read_html(.url)
-  block <- page %>% html_nodes(xpath = "//td[@class='BlackText']")
+    ## read html
+    page <- read_html(.url)
+    block <- page %>% html_nodes(xpath = "//td[@class='BlackText']")
 
-  # keep nodesets that contains information
-  block <- block[c(1, 3)]
+    ## keep nodesets that contains information
+    block <- block[c(1, 3)]
 
-  veteran_name <- block[1] %>%
-    html_text() %>%
-    str_replace_all("[\r\n\t]", "") %>%
-    str_trim(side = "both")
+    veteran_name <- block[1] %>%
+        html_text() %>%
+        str_replace_all("[\r\n\t]", "") %>%
+        str_trim(side = "both")
 
-  veteran_profile <- block[[2]] %>%
-    as.character() %>%
-    str_split("<br>") %>%
-    flatten_chr() %>%
-    map_chr(str_replace_all, pattern = "[\r\n\t]|(<!--|-->)|(<.*>)", replacement = "") %>%
-    map_chr(str_trim, side = "both") %>%
-    keep(~ .x != "")
+    veteran_profile <- block[[2]] %>%
+        as.character() %>%
+        str_split("<br>") %>%
+        flatten_chr() %>%
+        map_chr(str_replace_all, pattern = "[\r\n\t]|(<!--|-->)|(<.*>)", replacement = "") %>%
+        map_chr(str_trim, side = "both") %>%
+        keep(~ .x != "")
 
-  c(veteran_name, veteran_profile)
+    c(veteran_name, veteran_profile)
 
 }
 
@@ -39,6 +39,9 @@ safe_crawler <- purrr::safely(crawler)
 vnm_memorial <- vector("list", length = n_pages)
 
 for (i in seq_along(vnm_memorial)) {
-  vnm_memorial[[i]] <- crawler(urls[i])
-  Sys.sleep(2)
+    vnm_memorial[[i]] <- safe_crawler(urls[i])
+    Sys.sleep(2)
 }
+
+## export sample data
+## saveRDS(vnm_memorial, file = "thewall_usa.rds")
