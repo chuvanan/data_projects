1569565259ab68509bd7e4be9f318b81e979cc0a
AnChu <chuvanan.cva@gmail.com>
2018-06-22 23:09:29 +0700

    Update script docs and rename objects

--- vnexpress/scripts/spider.R
+++ vnexpress/scripts/spider.R
@@ -1,10 +1,15 @@
-##
+## A short script to pull data from vnexpress.net
+## @anchu
+## 22/06/2018
 
 
 
 library(xml2)
 library(purrr)
 library(stringr)
+
+## https://vnexpress.net/robots.txt
+## Allow: /
 
 ## -----------------------------------------------------------------------------
 ## extract daily sitemap-news urls
@@ -15,12 +20,12 @@ sm_xml <- vnex_sitemap %>%
     read_xml() %>%
     xml_contents()
 
-sm_node <- sm_xml %>%
+sm_nodes <- sm_xml %>%
     map(as.character) %>%
     map(str_split, "\n") %>%
     map(flatten_chr)
 
-sm_link <- sm_node %>%
+sm_links <- sm_nodes %>%
     map(function(x) keep(x, str_detect(x, "loc"))) %>%
     map_chr(function(x) str_replace_all(x, "\\s|</?loc>", "")) %>%
     keep(function(x) str_detect(x, "sitemap-news"))
