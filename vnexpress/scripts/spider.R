6226a312a908e5d74f11c8b4289ca2a5078944e0
AnChu <chuvanan.cva@gmail.com>
2018-06-23 14:06:12 +0700

    Pull and export vnex-headlines csv

--- vnexpress/scripts/spider.R
+++ vnexpress/scripts/spider.R
@@ -3,33 +3,56 @@
 ## 22/06/2018
 
 
-
 library(xml2)
 library(purrr)
 library(stringr)
+source("helper-functions.R")
 
 ## https://vnexpress.net/robots.txt
 ## Allow: /
 
-## -----------------------------------------------------------------------------
 ## extract daily sitemap-news urls
 
 vnex_sitemap <- "https://vnexpress.net/sitemap/1000000/sitemap.xml"
 
-sm_xml <- vnex_sitemap %>%
-    read_xml() %>%
-    xml_contents()
+sm_links <- get_sm_link(vnex_sitemap)
 
-sm_nodes <- sm_xml %>%
-    map(as.character) %>%
-    map(str_split, "\n") %>%
-    map(flatten_chr)
-
-sm_links <- sm_nodes %>%
-    map(function(x) keep(x, str_detect(x, "loc"))) %>%
-    map_chr(function(x) str_replace_all(x, "\\s|</?loc>", "")) %>%
-    keep(function(x) str_detect(x, "sitemap-news")) %>%
-    map_chr(function(x) str_replace_all(x, "amp;", ""))
-
-## -----------------------------------------------------------------------------
 ## extract daily headlines, publication dates, images, etc.
+
+dta <- map(sm_links, possibly_parse_sm_link)
+dta <- discard(dta, is.null)
+
+## found two corrupted links:
+## "https://vnexpress.net/sitemap/1000000/sitemap-news.xml?y=2014&m=11&d=27"
+## "https://vnexpress.net/sitemap/1000000/sitemap-news.xml?y=2014&m=11&d=11"
+
+fix_links <- c("https://vnexpress.net/sitemap/1000000/sitemap-news.xml?y=2014&m=11&d=27",
+               "https://vnexpress.net/sitemap/1000000/sitemap-news.xml?y=2014&m=11&d=11")
+
+dta_fix <- map(fix_links, function(x) {
+    con <- curl::curl(x)
+    xml <- readLines(con)
+    close(con)
+
+    url_loc <- get_url_loc(xml)
+    url_loc <- str_replace_all(url_loc, "<url>", "")
+    change_freq <- get_change_freq(xml)
+    priority <- get_priority(xml)
+    image_loc <- get_image_loc(xml)
+    image_title <- get_image_title(xml)
+    news_name <- get_news_name(xml)
+    news_language <- get_news_language(xml)
+    news_publication_date <- get_news_publication_date(xml)
+    news_title <- get_news_title(xml)
+    news_keywords <- get_news_keywords(xml)
+    tibble::data_frame(url_loc, change_freq, image_loc, image_title,
+                       news_name, news_language, news_publication_date,
+                       news_title, news_keywords)
+})
+
+full_dta <- append(dta, dta_fix)
+full_dta <- dplyr::bind_rows(full_dta)
+
+## export csv
+write.csv(full_dta, "./data/vnex-headlines.csv", row.names = FALSE)
+saveRDS(full_dta, "~/Documents/data_projects/vnexpress/data/vnex-headlines.RDS")
