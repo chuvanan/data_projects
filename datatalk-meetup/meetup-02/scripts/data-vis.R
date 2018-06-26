e02d1ae4bc140cc34fe306f0086b19d1eeea8e6e
An Chu <chuvanan.cva@gmail.com>
2018-06-26 12:34:17 +0700

    Call gridExtra to draw legend table

--- datatalk-meetup/meetup-02/scripts/data-vis.R
+++ datatalk-meetup/meetup-02/scripts/data-vis.R
@@ -1,6 +1,7 @@
 
 library(dplyr)
 library(ggplot2)
+library(gridExtra)
 
 setwd("~/Documents/data_projects/datatalk-meetup/meetup-02/")
 air_quality <- readr::read_csv("./data/hanoi-air-quality.csv")
@@ -29,6 +30,19 @@ eom <- RTA::end_of_month(seq.Date(from = as.Date('2018-01-01'),
                                   by = '1 month', length.out = 12L))
 eom <- lubridate::yday(eom)
 
+
+## table legend
+aqi_table <- data_frame(
+    AQI = c("300+", "201-300", "151-200", "101-150", "51-100", "0-50"),
+    `Pollution Level` = c("Hazardous", "Very unhealthy", "Unhealthy",
+                          "Unhealthy for sensitive groups", "Moderate",
+                          "Good")
+)
+aqi_table <- tableGrob(aqi_table, rows = NULL,
+                       theme = ttheme_minimal(base_family = "Roboto Slab",
+                                              base_colour = "gray30",
+                                              base_size = 13L))
+
 p <- ggplot(new_air) +
     geom_hline(yintercept = c(50, 100, 150, 200, 300), color = 'gray70',
                size = 0.5, linetype = 3) +
@@ -37,26 +51,26 @@ p <- ggplot(new_air) +
     geom_linerange(aes(day_th, ymin = min_aqi, ymax = max_aqi),
                    color = 'wheat2') +
     geom_linerange(aes(day_th, ymin = lower_aqi, ymax = upper_aqi),
-                   color = 'gray1') +
-    labs(x = NULL, y = "Air Quality Index (AQI)",
-         title = 'The polluted city',
-         subtitle = "The graph shows Hanoi's daily outdoor air quality from Dec, 2015 to Jun, 2018",
-         caption = "Data source: U.S. Embassy Hanoi Air Quality Monitor") +
-    scale_y_continuous(breaks = c(50, 100, 150, 200, 300)) +
-    scale_x_continuous(breaks = c(15,45,75,105,135,165,195,228,258,288,320,350),
-                       labels = c("January", "February", "March", "April",
-                                  "May", "June", "July", "August", "September",
-                                  "October", "November", "December"),
-                       expand = c(0, 0)) +
-    theme_minimal(base_family = 'Roboto Slab') +
-    theme(panel.grid = element_blank(),
-          plot.title = element_text(size = 30L),
-          plot.subtitle = element_text(size = 15L, color = 'gray30'),
-          axis.text.x = element_text(size = 15L, color = "black"),
-          axis.text.y = element_text(size = 13),
-          axis.title.y = element_text(size = 13, color = "gray30", vjust = 4),
-          plot.caption = element_text(size = 14L, vjust = -4, color = "gray30"),
-          plot.margin = unit(c(1.2, 1, 1, 1), "cm"))
+                       color = 'gray1') +
+        labs(x = NULL, y = "Air Quality Index (AQI)",
+             title = 'The polluted city',
+             subtitle = "The graph shows Hanoi's daily outdoor air quality from Dec, 2015 to Jun, 2018",
+             caption = "Data source: U.S. Embassy Hanoi Air Quality Monitor") +
+        scale_y_continuous(breaks = c(50, 100, 150, 200, 300)) +
+        scale_x_continuous(breaks = c(15,45,75,105,135,165,195,228,258,288,320,350),
+                           labels = c("January", "February", "March", "April",
+                                      "May", "June", "July", "August", "September",
+                                      "October", "November", "December"),
+                           expand = c(0, 0)) +
+        theme_minimal(base_family = 'Roboto Slab') +
+        theme(panel.grid = element_blank(),
+              plot.title = element_text(size = 30L),
+              plot.subtitle = element_text(size = 15L, color = 'gray30'),
+              axis.text.x = element_text(size = 15L, color = "black"),
+              axis.text.y = element_text(size = 13),
+              axis.title.y = element_text(size = 13, color = "gray30", vjust = 4),
+              plot.caption = element_text(size = 14L, vjust = -4, color = "gray30"),
+              plot.margin = unit(c(1.2, 1, 1, 1), "cm"))
 p + geom_linerange(aes(x = 100, ymin = 250, ymax = 350),
                    size = 2, color = "wheat2") +
     geom_linerange(aes(x = 100, ymin = 280, ymax = 320),
@@ -71,13 +85,7 @@ p + geom_linerange(aes(x = 100, ymin = 250, ymax = 350),
              size = 4.5, color = "gray30") +
     annotate("segment", x = 102.5, xend = 104, y = 280, yend = 280, color = "gray30", size = 0.35) +
     annotate("segment", x = 102.5, xend = 104, y = 320, yend = 320, color = "gray30", size = 0.35) +
-    annotate("text", 190, 370, label = "Pollution Level:", size = 4.5, color = "gray10", hjust = 0) +
-    annotate("text", 190, 350, label = "     300+: Hazardous", size = 4.5, color = "gray30", hjust = 0) +
-    annotate("text", 190, 330, label = "201-300: Very Unhealthy", size = 4.5, color = "gray30", hjust = 0) +
-    annotate("text", 190, 310, label = "151-200: Unhealthy", size = 4.5, color = "gray30", hjust = 0) +
-    annotate("text", 190, 290, label = "101-150: Unhealthy for Sensitive Groups", size = 4.5, color = "gray30", hjust = 0) +
-    annotate("text", 190, 270, label = "  51-100: Moderate", size = 4.5, color = "gray30", hjust = 0) +
-    annotate("text", 190, 250, label = "      0-50: Good", size = 4.5, color = "gray30", hjust = 0)
+    annotation_custom(aqi_table, xmin = 200, xmax = 230, ymin = 260, ymax = 340)
 
 dev.size("in")
 ## [1] 15.38542  6.48294
