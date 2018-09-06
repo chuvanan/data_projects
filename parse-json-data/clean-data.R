092007e808935ad2062c2bd25fcacca01b255a29
AnChu <chuvanan.cva@gmail.com>
2018-09-06 23:29:15 +0700

    Update code to export data

--- parse-json-data/clean-data.R
+++ parse-json-data/clean-data.R
@@ -25,10 +25,29 @@ sample_dta <- dta[sample(1:nrow(dta), size = 10000)]
 
 
 ggplot(by_month, aes(factor(month), N, fill = factor(year))) +
-    geom_col() +
+    geom_col(show.legend = FALSE) +
     facet_wrap( ~ factor(year), scales = "free_y") +
     labs(x = NULL) +
     scale_fill_manual(name = NULL, values = c("firebrick", "steelblue"))
 
 ## ggsave("~/Documents/data_projects/parse-json-data/by_month.png")
 ## fwrite(sample_dta, "~/Documents/data_projects/parse-json-data/sample-bds-data.csv")
+
+fwrite(dta[year == 2017 & month == 1], "bds-201701.csv")
+fwrite(dta[year == 2017 & month == 2], "bds-201702.csv")
+fwrite(dta[year == 2017 & month == 3], "bds-201703.csv")
+fwrite(dta[year == 2017 & month == 4], "bds-201704.csv")
+fwrite(dta[year == 2017 & month == 5], "bds-201705.csv")
+fwrite(dta[year == 2017 & month == 6], "bds-201706.csv")
+fwrite(dta[year == 2017 & month == 7], "bds-201707.csv")
+fwrite(dta[year == 2017 & month == 8], "bds-201708.csv")
+fwrite(dta[year == 2017 & month == 9], "bds-201709.csv")
+fwrite(dta[year == 2017 & month == 10], "bds-201710.csv")
+fwrite(dta[year == 2017 & month == 11], "bds-201711.csv")
+fwrite(dta[year == 2017 & month == 12], "bds-201712.csv")
+fwrite(dta[year == 2018 & month == 1], "bds-201801.csv")
+fwrite(dta[year == 2018 & month == 2], "bds-201802.csv")
+fwrite(dta[year == 2018 & month == 3], "bds-201803.csv")
+fwrite(dta[year == 2018 & month == 4], "bds-201804.csv")
+fwrite(dta[year == 2018 & month == 5], "bds-201805.csv")
+fwrite(dta[year == 2018 & month == 6], "bds-201806.csv")
