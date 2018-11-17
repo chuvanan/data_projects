b2cac35e4e51305db95ebbb92cedf96c901c8d3b
AnChu <chuvanan.cva@gmail.com>
2018-11-17 19:11:44 +0700

    First six questions

--- rise_and_fall_of_programming_langs/rise-fall-programming-langs.R
+++ rise_and_fall_of_programming_langs/rise-fall-programming-langs.R
@@ -5,5 +5,44 @@
 
 library(readr)
 library(dplyr)
+library(ggplot2)
 
 by_tag_year <- read_csv("./data/by_tag_year.csv")
+
+names(by_tag_year)
+dim(by_tag_year)
+head(by_tag_year)
+
+## -----------------------------------------------------------------------------
+## Data exploration
+
+
+## Add fraction column
+by_tag_year_fraction <- by_tag_year %>%
+    mutate(fraction = number / year_total)
+
+
+## Has R been growing or shrinking?
+
+r_over_time <- by_tag_year_fraction %>%
+    filter(tag == "r")
+
+## Visualizing change over time
+ggplot(r_over_time, aes(year, fraction)) +
+    geom_line()
+
+
+## How about dplyr and ggplot2
+selected_tags <- c("r", "dplyr", "ggplot2")
+selected_tags_over_time <- by_tag_year_fraction %>%
+    filter(tag %in% selected_tags)
+
+ggplot(selected_tags_over_time, aes(year, fraction, color = tag)) +
+    geom_line()
+
+## What are the most asked-about tags?
+sorted_tags <- by_tag_year_fraction %>%
+    group_by(tag) %>%
+    summarise(tag_total = sum(number)) %>%
+    ungroup() %>%
+    arrange(desc(tag_total))
