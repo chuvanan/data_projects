39df62367ded02e4571cdd40f2d11187e77011c5
AnChu <chuvanan.cva@gmail.com>
2019-03-02 21:18:31 +0700

    EGOT winners scatter plot

--- EGOT/EGOT-timeline.R
+++ EGOT/EGOT-timeline.R
@@ -1,9 +1,17 @@
 
 
 library(ggplot2)
+library(tidyr)
 library(dplyr)
+library(ggrepel)
+library(gridExtra)
+library(hrbrthemes)
 
-egot <- readr::read_csv("EGOT-winners.csv")
+egot_raw <- readr::read_csv("EGOT-winners.csv")
+
+egot <- egot_raw %>%
+    select(winner = name, emmy, grammy, oscar, tony) %>%
+    gather(prize, year, -winner)
 
 egot <- egot %>%
     mutate(winner = forcats::fct_reorder(winner, year, max),
@@ -35,3 +43,66 @@ egot_timeline <- ggplot(aes(year, winner,
     bigger_font_theme()
 
 ## ggsave(filename = "EGOT-timeline.pdf", egot_timeline)
+
+emmy <- egot %>%
+    filter(prize == "emmy") %>%
+    mutate(winner = forcats::fct_reorder(winner, year),
+           winner = forcats::fct_rev(winner)) %>%
+    ggplot(aes(year, winner, label = winner)) +
+    geom_point() +
+    geom_text_repel() +
+    labs(x = NULL, y = NULL) +
+    scale_x_continuous(breaks = seq(1930, 2010, 10)) +
+    theme_minimal() +
+    theme(axis.text.y = element_blank())
+
+grammy <- egot %>%
+    filter(prize == "grammy") %>%
+    mutate(winner = forcats::fct_reorder(winner, year),
+           winner = forcats::fct_rev(winner)) %>%
+    ggplot(aes(year, winner, label = winner)) +
+    geom_point() +
+    geom_text_repel() +
+    labs(x = NULL, y = NULL) +
+    scale_x_continuous(breaks = seq(1930, 2010, 10)) +
+    theme_minimal() +
+    theme(axis.text.y = element_blank())
+
+oscar <- egot %>%
+    filter(prize == "oscar") %>%
+    mutate(winner = forcats::fct_reorder(winner, year),
+           winner = forcats::fct_rev(winner)) %>%
+    ggplot(aes(year, winner, label = winner)) +
+    geom_point() +
+    geom_text_repel() +
+    labs(x = NULL, y = NULL) +
+    scale_x_continuous(breaks = seq(1930, 2010, 10)) +
+    theme_minimal() +
+    theme(axis.text.y = element_blank())
+
+tony <- egot %>%
+    filter(prize == "tony") %>%
+    mutate(winner = forcats::fct_reorder(winner, year),
+           winner = forcats::fct_rev(winner)) %>%
+    ggplot(aes(year, winner, label = winner)) +
+    geom_point() +
+    geom_text_repel() +
+    labs(x = NULL, y = NULL) +
+    scale_x_continuous(breaks = seq(1930, 2010, 10)) +
+    theme_minimal() +
+    theme(axis.text.y = element_blank())
+
+grid.arrange(emmy, grammy, oscar, tony)
+
+egot_winners <- egot_raw %>%
+    ggplot(aes(year_span, age, label = name)) +
+    geom_smooth(se = FALSE, method = "lm", color = "lightblue") +
+    geom_point() +
+    geom_text_repel(size = 4, color = "white") +
+    scale_color_ipsum() +
+    labs(x = "years taken to win all four awards",
+         y = "age at the time of completion",
+         title = "EGOT Winners") +
+    theme_modern_rc()
+
+ggsave(filename = "EGOT-winners.pdf", egot_winners)
