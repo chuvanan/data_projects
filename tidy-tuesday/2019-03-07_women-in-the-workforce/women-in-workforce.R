a151b7cbb6298d68916b9912b017c826b56c0a6f
anchu <chuvanan.cva@gmail.com>
2019-03-07 13:15:02 +0700

    Employment trend of US workforce

--- tidy-tuesday/2019-03-07_women-in-the-workforce/women-in-workforce.R
+++ tidy-tuesday/2019-03-07_women-in-the-workforce/women-in-workforce.R
@@ -1,5 +1,5 @@
 
-
+library(ggplot2)
 
 ## Get the data
 jobs_gender <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-03-05/jobs_gender.csv")
@@ -20,5 +20,31 @@ dim(employed_gender)
 head(employed_gender)
 sapply(employed_gender, class)
 
+
+## -----------------------------------------------------------------------------
+## helper functions
+
+bigger_font <- function() {
+    theme(text = element_text(size = 15))
+}
+
+
 ## -----------------------------------------------------------------------------
 ## explore employed_gender
+
+## Are more people working full time or part time over the years?
+
+employment_trend <- ggplot(employed_gender) +
+    geom_line(aes(year, total_full_time, color = "fulltime"), size = 1.5) +
+    geom_line(aes(year, total_part_time, color = "parttime"), size = 1.5) +
+    scale_y_continuous(label = scales::percent_format(scale = 1)) +
+    scale_color_manual(name = NULL,
+                       values = c(fulltime = "red4", parttime = "blue")) +
+    labs(x = NULL, y = "Percentage of employed people",
+         title = "Employment Trend in the U.S.A") +
+    bigger_font() +
+    theme(legend.position = "bottom")
+
+## ggsave(filename = "employment-trend.pdf", employment_trend)
+## slight decrease in full-time workforce and a gradual increase of
+## part-time workers
