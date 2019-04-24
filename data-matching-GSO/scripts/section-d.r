90887c5bb0a94f70dba10211e07dccfb14ec2900
anchu <chuvanan.cva@gmail.com>
2019-04-24 10:27:01 +0700

    Update section-d, recalculate the perct of the 2nd table

--- data-matching-GSO/scripts/section-d.r
+++ data-matching-GSO/scripts/section-d.r
@@ -7,6 +7,8 @@ source("02-process.R")
 
 ## D1 ------------------------------
 
+## THỰC TRẠNG ĐỂ DÀNH TIỀN/TIẾT KIỆM TIỀN
+
 fisf <- fisf %>%
     mutate(D1_1_LABEL = onehot_encoding(select(fisf, matches("D1_[0-9]$")), 1)) %>%
     mutate(D1_2_LABEL = onehot_encoding(select(fisf, matches("D1_[0-9]$")), 2)) %>%
@@ -43,6 +45,8 @@ D1 <- bind_cols(
 
 ## D2 ------------------------------
 
+## TẦN SUẤT ĐỂ DÁNH TIỀN/TIẾT KIỆM TIỀN TRONG 12 THÁNG QUA
+
 fisf <- fisf %>%
     mutate(D2 = fill_missval(D2))
 
@@ -75,6 +79,8 @@ D1_2 <- bind_cols(
 
 ## D3 ------------------------------
 
+## HÌNH THỨC TIẾT KIỆM
+
 fisf <- fisf %>%
     mutate(D3_1_LABEL = onehot_encoding(select(fisf, matches("D3_[0-9]$")), 1)) %>%
     mutate(D3_2_LABEL = onehot_encoding(select(fisf, matches("D3_[0-9]$")), 2)) %>%
@@ -96,16 +102,16 @@ fisf <- fisf %>%
         ifelse(. == 1, "Có", "Không")
     })
 
-D3_1 <- count_response(fisf, D3_1_LABEL, index = "D3")
-D3_2 <- count_response(fisf, D3_2_LABEL, index = "D3_2")
-D3_3 <- count_response(fisf, D3_3_LABEL, index = "D3_3")
-D3_4 <- count_response(fisf, D3_4_LABEL, index = "D3_4")
-D3_5 <- count_response(fisf, D3_5_LABEL, index = "D3_5")
-D3_6 <- count_response(fisf, D3_6_LABEL, index = "D3_6")
-D3_7 <- count_response(fisf, D3_7_LABEL, index = "D3_7")
+D3_1 <- count_response(filter(fisf, D1_7_LABEL != "Không"), D3_1_LABEL, index = "D3")
+D3_2 <- count_response(filter(fisf, D1_7_LABEL != "Không"), D3_2_LABEL, index = "D3_2")
+D3_3 <- count_response(filter(fisf, D1_7_LABEL != "Không"), D3_3_LABEL, index = "D3_3")
+D3_4 <- count_response(filter(fisf, D1_7_LABEL != "Không"), D3_4_LABEL, index = "D3_4")
+D3_5 <- count_response(filter(fisf, D1_7_LABEL != "Không"), D3_5_LABEL, index = "D3_5")
+D3_6 <- count_response(filter(fisf, D1_7_LABEL != "Không"), D3_6_LABEL, index = "D3_6")
+D3_7 <- count_response(filter(fisf, D1_7_LABEL != "Không"), D3_7_LABEL, index = "D3_7")
 
-D3_2UNIQ <- count_response(fisf, D3_2_UNIQ_LABEL, index = "D3_2")
-D3_5UNIQ <- count_response(fisf, D3_5_UNIQ_LABEL, index = "D3_5")
+D3_2UNIQ <- count_response(filter(fisf, D1_7_LABEL != "Không"), D3_2_UNIQ_LABEL, index = "D3_2")
+D3_5UNIQ <- count_response(filter(fisf, D1_7_LABEL != "Không"), D3_5_UNIQ_LABEL, index = "D3_5")
 
 D3 <- bind_cols(
     select(D3_1, STT, `Phân loại ngừơi trả lời`, `Giữ tại nhà` = `Có`),
