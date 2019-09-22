06c97a486f3bc94db81160f6d1a290385565f8af
An Chu <chuvanan.cva@gmail.com>
2019-09-22 21:13:26 +0700

    Clean air quality data, export to .rds

--- hanoi-aqi-2019/scripts/clean-air-quality-data.r
+++ hanoi-aqi-2019/scripts/clean-air-quality-data.r
@@ -18,6 +18,26 @@ dim(aqi_ytd_2017)
 dim(aqi_ytd_2018)
 dim(aqi_ytd_2019)
 
+## Helper functions ------------------------------------------------------------
+
+categorize_aqi <- function(aqi) {
+    if (aqi <= 50) {
+        out <- "Good"
+    } else if (aqi >= 51 & aqi <= 100) {
+        out <- "Moderate"
+    } else if (aqi >= 101 & aqi <= 150) {
+        out <- "Unhealthy for Sensitive Groups"
+    } else if (aqi >= 151 & aqi <= 200) {
+        out <- "Unhealthy"
+    } else if (aqi >= 201 & aqi <= 300) {
+        out <- "Very Unhealthy"
+    } else if (aqi >= 301) {
+        out <- "Hazardous"
+    }
+    return(out)
+}
+
+
 ## Combine data sets into one --------------------------------------------------
 
 ## check that all files have idential column names
@@ -46,3 +66,34 @@ all.equal(sapply(aqi_ytd_2015, class), sapply(aqi_ytd_2019, class))
 ## combine them all
 aqi <- bind_rows(aqi_ytd_2015, aqi_ytd_2016, aqi_ytd_2017,
                  aqi_ytd_2018, aqi_ytd_2019)
+
+## EDA -------------------------------------------------------------------------
+
+aqi$site <- NULL
+aqi$parameter <- NULL
+aqi$nowcast_conc[aqi$nowcast_conc == -999] <- NA
+aqi$aqi[aqi$aqi == -999] <- NA
+aqi$conc_unit <- NULL
+aqi$duration <- NULL
+aqi$aqi_categ[aqi$aqi_categ == "N/A"] <- NA
+aqi$raw_conc[aqi$raw_conc == -999] <- NA
+
+## remove missing records
+aqi <- aqi[!is.na(aqi$aqi), ]
+
+## parse dates
+aqi$date <- as.POSIXct(aqi$date, format = "%Y-%m-%d %I:%M %p")
+
+## confirm AQI levels are accurately classified
+aqi_levels <- sapply(aqi$aqi, categorize_aqi)
+table(aqi_levels == aqi$aqi_categ)
+
+aqi$aqi_categ <- factor(aqi$aqi_categ,
+                        levels = c("Good", "Moderate", "Unhealthy for Sensitive Groups",
+                                   "Unhealthy", "Very unhealthy", "Hazardous"),
+                        ordered = TRUE)
+table(aqi$aqi_categ) * 100 / nrow(aqi)
+
+## Export ----------------------------------------------------------------------
+
+## saveRDS(aqi, "./data/hanoi-air-quality.rds")
