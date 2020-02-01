a54194a2e3e950b73acfaf2d614cef73e928543f
AnChu <chuvanan.cva@gmail.com>
2020-02-01 21:32:50 +0700

    Update data 31/01/2020

--- wuhan-coronavirus-outbreak/01-crawl.r
+++ wuhan-coronavirus-outbreak/01-crawl.r
@@ -1,5 +1,6 @@
 
 
+
 require(rvest) # for reading html page
 
 wiki_page <- "https://en.wikipedia.org/wiki/2019%E2%80%9320_Wuhan_coronavirus_outbreak"
@@ -12,9 +13,10 @@ tail(outbreak)
 
 ## rename columns
 names(outbreak) <- c("country", "confirmed_cases", "deaths", "reference")
+outbreak <- outbreak[, c("country", "confirmed_cases", "deaths", "reference")]
 
 ## remove rows
-outbreak <- outbreak[!grepl(pattern = "territories|As of", outbreak$country), ]
+outbreak <- outbreak[!grepl(pattern = "territories|As of|vte", outbreak$country), ]
 
 ## clean data
 outbreak$confirmed_cases <- gsub(pattern = "\\[[0-9]+\\]|,", replacement = "", outbreak$confirmed_cases)
@@ -25,10 +27,10 @@ outbreak <- outbreak[order(outbreak$confirmed_cases), ]
 
 ## compare countries
 png(filename = "./wuhan-virus-outbreak.png", 7, 7, units = "in", res = 300)
-par(mar = c(3, 9.5, 2, 2), family = "Nunito")
+par(mar = c(3, 9.5, 2, 2), family = "Inconsolata")
 barplot(outbreak$confirmed_cases, names.arg = outbreak$country, horiz = TRUE, col = "goldenrod", border = "goldenrod", las = 1,
-        main = "Wuhan CoronaVirus Outbreak by Territory")
+        main = "Wuhan nCoV Outbreak by Territory (31/01/2020)")
 barplot(height = c(rep(NA, NROW(outbreak) - 1), max(outbreak$deaths)), col = "red", add = TRUE, horiz = TRUE, border = "red", axes = FALSE)
-abline(v = seq(2e3, 8e3, 2e3), col = "white", lwd = 2)
+abline(v = seq(2e3, 10e3, 2e3), col = "white", lwd = 2)
 legend("center", legend = c("confirmed cases", "deaths"), col = c("goldenrod", "red"), pch = 15, bty = "n")
 dev.off()
