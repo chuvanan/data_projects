9698a963d86b0eaf8fb206996761fbf64d61ff82
AnChu <chuvanan.cva@gmail.com>
2020-02-07 22:54:57 +0700

    Update public debt ratio chart. Add Vietnam

--- debt-gdp-ratio/02-visualise-pubic-debt.r
+++ debt-gdp-ratio/02-visualise-pubic-debt.r
@@ -2,11 +2,13 @@
 public_debt <- read.csv("./public-debt-ratio.csv", stringsAsFactors = FALSE)
 public_debt$region <- factor(public_debt$region)
 
-high_risk_conds <- public_debt$debt_gdp_ratio > 100 | public_debt$share_of_debt > 10
-top_risk <- c("United States", "Japan", "China", "Italy", "Greece")
+high_risk_conds <- public_debt$debt_gdp_ratio > 100 | public_debt$share_of_debt > 10 | public_debt$country == "Vietnam"
+top_risk <- c("United States", "Japan", "China", "Italy", "Greece", "Vietnam")
 public_debt$high_risk <- ifelse(high_risk_conds, "red", "darkgray")
 
-par(family = "Roboto Condensed", cex = 1.4, mar = c(4.1, 4.1, 4, 2), xpd = TRUE)
+
+png(filename = "./public-debt-ratio.png", 7, 7, units = "in", res = 300)
+par(family = "Carlito", cex = 1.1, mar = c(4.1, 4.1, 4, 2), xpd = TRUE)
 plot(public_debt$debt_gdp_ratio,
      public_debt$share_of_debt, las = 1,
      pch = 21, bg = public_debt$high_risk, col = "white",
@@ -14,4 +16,5 @@ plot(public_debt$debt_gdp_ratio,
      main = "Percentage of debt vs. gdp/debt by country")
 text(public_debt$debt_gdp_ratio[public_debt$country %in% top_risk],
      public_debt$share_of_debt[public_debt$country %in% top_risk],
-     labels = public_debt$country[public_debt$country %in% top_risk], pos = 4)
+     labels = public_debt$country[public_debt$country %in% top_risk], pos = 3)
+dev.off()
